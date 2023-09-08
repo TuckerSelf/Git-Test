14769036c333a595330e24a2ae77207d3ace4e41
TuckerSelf <tself3@ut.utm.edu>
2023-09-07 20:10:41 -0500

    first main commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file main.cpp
+ * @author Tucker Self
+ * @date 2023-09-07
+ * @brief simple code test
+ * 
+ * done to get used to using emacs and git
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << endl;
+  
+  return 0;
+}
